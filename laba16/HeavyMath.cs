1127b9929a03f00a942848955156111a88431474
Andrei <v.andreihaneckii@gmail.com>
2021-02-05 19:22:34 +0300

    done

---
+++ laba16/HeavyMath.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace laba16
+{
+    class HeavyMath
+    {
+        public static void MatrixMultiply()
+        {
+            int size = 8000;
+            int[,] matrix = new int[size, size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = rnd.Next(10, 100);
+                }
+            }
+
+            int[,] matrix2 = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix2[i, j] = rnd.Next(10, 100);
+                }
+            }
+
+            int[,] matrix3 = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix3[i, j] = matrix[i, j] * matrix[i, j];
+                }
+            }
+
+            Console.WriteLine("End.");
+        }
+
+        public static int Multi(int x, int y)
+        {
+            int result;
+            result = x * y;
+            return result;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            int result;
+            result = x / y;
+            return result;
+        }
+
+        public static int Minus(int x, int y)
+        {
+            int result;
+            result = x - y;
+            return result;
+        }
+
+        public static void Output(int output)
+        {
+            Console.WriteLine($"Результат: {output}");
+        }
+
+        public static void ReallyBigArray(int multi)
+        {
+            int size = 1000000 * multi;
+            int[] array = new int[size];
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = rnd.Next(10, 100);
+            }
+        }
+
+        public static void Goods()
+        {
+            Task p1 = new Task(producer);
+            Task p2 = new Task(producer);
+            Task p3 = new Task(producer);
+            Task p4 = new Task(producer);
+            Task p5 = new Task(producer);
+
+            Task c1 = new Task(consumer);
+            Task c2 = new Task(consumer);
+            Task c3 = new Task(consumer);
+            Task c5 = new Task(consumer);
+            Task c6 = new Task(consumer);
+            Task c7 = new Task(consumer);
+            Task c8 = new Task(consumer);
+            Task c9 = new Task(consumer);
+            Task c10 = new Task(consumer);
+
+            p1.Start();
+            p2.Start();
+            p3.Start();
+            p4.Start();
+            p5.Start();
+            c1.Start();
+            c2.Start();
+            c3.Start();
+            c5.Start();
+            c6.Start();
+            c7.Start();
+            c8.Start();
+            c9.Start();
+            c10.Start();
+        }
+        static int mod = 1;
+        static BlockingCollection<int> bc = new BlockingCollection<int>(5);
+        static void producer()
+        {
+            mod++;
+            for (int i = (mod - 1); i < mod; i++)
+            {
+                bc.Add(i);
+                Thread.Sleep(500);
+                Console.WriteLine("added: " + i);
+                foreach (var j in bc)
+                {
+                    Console.WriteLine("amount of products: " + j);
+                    Thread.Sleep(500);
+                }
+                Thread.Sleep(500);
+            }
+        }
+        static void consumer()
+        {
+            mod++;
+            int i;
+            while (!bc.IsCompleted)
+            {
+                Thread.Sleep(500);
+                if (bc.TryTake(out i))
+                {
+                    Console.WriteLine("deleted: " + i);
+                    Thread.Sleep(500);
+                }
+                else
+                {
+                    if (i == 0)
+                    {
+                        Console.WriteLine("empty");
+                    }
+                }
+                Thread.Sleep(500);
+            }
+        }
+        public static async void BigArrayAsync()
+        {
+            await Task.Run(() => ReallyBigArray(100));                // выполняется асинхронно
+            Console.WriteLine("Конец метода BigArrayAsync");
+        }
+    }
+}
---
+++ laba16/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace laba16
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task task1 = new Task(HeavyMath.MatrixMultiply);
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            task1.Start();
+            watch.Stop();
+            TimeSpan time = watch.Elapsed;
+            Console.WriteLine("RunTime " + time);
+            Console.WriteLine($"My task: {task1.Id} - {task1.Status}");
+            task1.Wait();
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+            Task task2 = new Task(() =>
+            {
+                int size = 8000;
+                int[,] matrix = new int[size, size];
+                Random rnd = new Random();
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        matrix[i, j] = rnd.Next(10, 100);
+                    }
+                }
+
+                int[,] matrix2 = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        matrix2[i, j] = rnd.Next(10, 100);
+                    }
+                }
+
+                int[,] matrix3 = new int[size, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        matrix3[i, j] = matrix[i, j] * matrix[i, j];
+                    }
+                }
+
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция прервана токеном");
+                    return;
+                }
+
+                Console.WriteLine("End.");
+            });
+
+            if (Console.ReadLine() == "q")
+            {
+                cancelTokenSource.Cancel();
+            }
+
+            
+            Task<int> Task1 = new Task<int>(() => HeavyMath.Multi(6, 6));
+            Task1.Start();
+
+            Task<int> Task2 = new Task<int>(() => HeavyMath.Divide(6, 3));
+            Task2.Start();
+
+            Task<int> Task3 = new Task<int>(() => HeavyMath.Minus(24, 12));
+            Task3.Start();
+
+            Task<int> Task4 = new Task<int>(() => Task1.Result + Task2.Result + Task3.Result);
+            Task4.Start();
+
+            Console.WriteLine(Task4.Result);
+
+            Task<int> task1_4 = new Task<int>(() => HeavyMath.Minus(24, 12));
+            Task tasK2_4 = task1_4.ContinueWith(mul => HeavyMath.Output(mul.Result));
+            task1_4.Start();
+            tasK2_4.Wait();
+
+            var awaiter = task1_4.GetAwaiter();
+
+            awaiter.OnCompleted(() =>
+            {
+                Console.WriteLine("Result: " + awaiter.GetResult());
+            });
+
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.For(0, 5, HeavyMath.ReallyBigArray);
+            stopwatch.Stop();
+            TimeSpan span = stopwatch.Elapsed;
+            Console.WriteLine($"Время: {span}");
+            Console.ReadKey();
+
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch.Start();
+            ParallelLoopResult result = Parallel.ForEach<int>(new List<int>() { 1, 3, 5, 8 },
+                HeavyMath.ReallyBigArray);
+            stopwatch.Stop();
+            TimeSpan span2 = stopwatch.Elapsed;
+            Console.WriteLine($"Время: {span2}");
+            Console.ReadKey();
+
+            Parallel.Invoke(
+                () => {
+                    Console.WriteLine($"Выполняется задача {Task.CurrentId}");
+                    Thread.Sleep(3000);
+                },
+                () => HeavyMath.ReallyBigArray(5));
+
+            Console.ReadKey();
+            HeavyMath.BigArrayAsync();
+            HeavyMath.Goods();
+
+            Console.ReadKey();
+        }
+    }
+}
